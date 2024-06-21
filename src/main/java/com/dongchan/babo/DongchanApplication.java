a24793ef5ea7e0486a35c0383270fffffc68570b
Sewook <sewook0303@dgsw.hs.kr>
2024-06-21 10:12:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/dongchan/babo/DongchanApplication.java
@@ -0,0 +1,13 @@
+package com.dongchan.babo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongchanApplication.class, args);
+	}
+
+}
